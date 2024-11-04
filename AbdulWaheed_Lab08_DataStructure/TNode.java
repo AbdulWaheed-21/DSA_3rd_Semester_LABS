a9a779cae97418e508ad45de813aff0fafafed2d
AbdulWaheed-21 <abwpak21@gmail.com>
2024-11-04 23:00:11 +0500

    This is my DSA Lab 8

---
+++ AbdulWaheed_Lab08_DataStructure/TNode.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labtask.tnode;
+
+/**
+ *
+ * @author Abdul Waheed
+ */
+public class TNode {
+ 
+    
+    
+     String data;
+       
+       TNode[] children;
+       
+       int childCount;
+       
+       public TNode(String val, int maxchildSize){
+       
+           this.data = val;
+           
+          this.children = new TNode[maxchildSize];
+       
+           this.childCount = 0;
+            
+       }
+
+        public void addChild(TNode child){
+          
+          
+          if(childCount<children.length){
+          
+          
+              children[childCount++] = child;
+              System.out.println(child.data+" have been added");
+          }
+          else{
+              System.out.println(" Node is already filled ");
+          
+          }
+         
+          }
+        
+        public void displayNode(TNode node ,int level ){
+        
+        if(node == null){
+        
+            return;
+        }
+        
+            System.out.println(level*1+" "+node.data);
+            
+            for(int i =0; i<node.childCount; i++){
+            
+                displayNode(node.children[i], level+1);
+            
+            }
+        
+        }
+
+       
+ 
+       }
+
+ 
---
+++ AbdulWaheed_Lab08_DataStructure/Tree.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package labtask.tnode;
+
+/**
+ *
+ * @author Abdul Waheed
+ */
+
+  public class Tree{
+       
+            TNode root;
+       
+          public Tree(String rootvalue , int maxChildSize){
+            
+                root = new TNode(rootvalue,maxChildSize);
+            }
+          
+         
+
+       
+       
+       
+      
+    public static void main(String[] args) {
+        Tree tree= new Tree("A", 2);
+        
+        TNode B = new TNode("B" ,3);
+        TNode C = new TNode("C" ,2);
+        
+        
+        tree.root.addChild(B);
+        tree.root.addChild(C);
+        
+        TNode D = new TNode("D" ,2);
+        TNode E = new TNode("E" ,2);
+        TNode F = new TNode("F" ,2);
+        
+        B.addChild(D);
+        B.addChild(E);
+        B.addChild(F);
+     
+    }
+}
